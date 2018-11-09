082111fcfebd9af3ba4f4c369daa6838c5a62079
stefegg <stephenegbert@gmail.com>
2018-11-09 11:25:14 -0500

    cleanup

--- depreciated/list.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const mongoose = require('mongoose');
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const config = require('../config/database');
-const listFile = require('../lib/find2.js');
-
-listFile.list()
--- depreciated/showstore.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const mongoose = require('mongoose');
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const config = require('../config/database');
-const showFile = require('../lib/show2.js');
-
-showFile.show(process.argv[2])
